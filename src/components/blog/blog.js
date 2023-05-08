0a495a463f9a74b80d76a0f3a43b2840fdb1d44a
Sosami <odunaikemotunrayo@gmail.com>
2023-05-08 12:28:59 +0100

    slider blog

--- src/components/blog/blog.js
+++ src/components/blog/blog.js
@@ -7,29 +7,14 @@ import Circle2 from '../../assets/services_circle.png';
 
 export default class Blog extends Component {
   render() {
-    const blogs = [
-      {
-        title: "Why Your SaaS Business should use WordPress",
-        content: "A content management system like WordPress can help you build a highly engaging website for your SaaS business, with little to no effort",
-      },
-      {
-        title: "Why Your SaaS Business should use WordPress",
-        content: "A content management system like WordPress can help you build a highly engaging website for your SaaS business, with little to no effort",
-      },
-      {
-        title: "Why Your SaaS Business should use WordPress",
-        content: "A content management system like WordPress can help you build a highly engaging website for your SaaS business, with little to no effort",
-      },
-    ];
 
-    var settings = {
-      dots: true,
+    const settings = {
+      dots: false,
       infinite: true,
-      speed: 500,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      centerMode: true,
-      // centerPadding: '60px',
+      slidesToShow: 2,
+      slidesToScroll: 3,
+      speed: 4000,
+      autoplaySpeed: 5000,
       autoplay: true,
       cssEase: 'linear' ,
 
@@ -60,15 +45,12 @@ export default class Blog extends Component {
             <button>See All</button>
           </div>
         </div>
-
-        <div className={classes.carousel}>
-          <div className={classes.cards}>
-            <Slider {...settings}>
-              {blogs.map((blog, index) => (
-                <div key={index} className={classes.card}>
+            <Slider {...settings} className={classes.cards}>
+                <div className={classes.card}>
+                  <div className={classes.innercard}>
                   <div className={classes.why}>
-                    <h3>{blog.title}</h3>
-                    <p>{blog.content}</p>
+                    <h3>Why Your SaaS Business should use WordPress</h3>
+                    <p>A content management system like WordPress can help you build a highly engaging website for your SaaS business, with little to no effort</p>
                     <div className={classes.moreArrow}>
                     <div className={classes.more}>
                       <p>Read More</p>
@@ -77,15 +59,42 @@ export default class Blog extends Component {
                      <img src={Arrow} alt='arrow' /> 
                     </div>
                     </div>
-                    
-                      
-                    
+                  </div>
                   </div>
                 </div>
-              ))}
-            </Slider>
-          </div>            
-        </div>
+                <div className={classes.card}>
+                  <div className={classes.innercard}>
+                  <div className={classes.why}>
+                    <h3>Why Your SaaS Business should use WordPress</h3>
+                    <p>A content management system like WordPress can help you build a highly engaging website for your SaaS business, with little to no effort</p>
+                    <div className={classes.moreArrow}>
+                    <div className={classes.more}>
+                      <p>Read More</p>
+                    </div>
+                    <div className={classes.arrow}>
+                     <img src={Arrow} alt='arrow' /> 
+                    </div>
+                    </div>
+                  </div>
+                  </div>
+                </div>
+                <div className={classes.card}>
+                  <div className={classes.innercard}>
+                  <div className={classes.why}>
+                    <h3>Why Your SaaS Business should use WordPress</h3>
+                    <p>A content management system like WordPress can help you build a highly engaging website for your SaaS business, with little to no effort</p>
+                    <div className={classes.moreArrow}>
+                     <div className={classes.more}>
+                       <p>Read More</p>
+                     </div>
+                     <div className={classes.arrow}>
+                      <img src={Arrow} alt='arrow' /> 
+                     </div>
+                    </div>
+                  </div>
+                  </div>
+                </div>
+            </Slider> 
 
         <div className={classes.circle1}>
           <img src={Circle1} alt='circle1' />
